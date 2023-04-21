72402bffc0b10044b876ea2934162735fa382a97
shirotamaki <shiro.tamaki63@gmail.com>
2023-04-21 09:38:23 +0900

    ♻️

--- next-rails-google-auth/backend/app/controllers/api/v1/users_controller.rb
+++ next-rails-google-auth/backend/app/controllers/api/v1/users_controller.rb
@@ -4,9 +4,8 @@ module Api
   module V1
     class UsersController < ApplicationController
       def create
-        user = User.find_or_create_by(provider: params[:provider], uid: params[:uid], name: params[:name],
-                                      email: params[:email])
-        if user.save
+        user = User.find_or_create_by(provider: params[:provider], uid: params[:uid], name: params[:name], email: params[:email])
+        if user
           head :ok
         else
           render json: { error: 'ログインに失敗しました' }, status: :unprocessable_entity
